0af48120d39c535034825408dc6449d6e1af4100
unknown <qkr5882103@gmail.com>
2021-02-21 00:37:03 +0900

    commit

--- src/models/User.js
+++ src/models/User.js
@@ -9,7 +9,7 @@ const UserSchema = new mongoose.Schema({
     introduction : String, // ex)배부른 개발자
     love : Boolean, // 연애 여부
     description: String, // 자기 소개글 ... (안녕하세요 26살 개발자 어쩌구저쩌구 ,,,,,,,,,,,)
-    nickname : String, // ex) @smp_2103 같은 아이디
+    nickname : {type:String, unique:true}, // ex) @smp_2103 같은 아이디
     email : {type:String,unique:true}, // 인증에 사용할 이메일
     location : String, // 서울특별시 서초구
     university: String, // 연세대학교 , 고려대학교 등
